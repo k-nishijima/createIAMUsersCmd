aa6968666c5a835cbcd7219b473b864f690e7237
Koichiro Nishijima <koichiro.nishijima@gmail.com>
2013-10-26 11:56:52 +0900

    取り敢えずやっつけで作成

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "awshandson-createUsers"
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
+
+libraryDependencies ++= Seq(
+    "com.amazonaws" % "aws-java-sdk" % "1.6.2"
+)
+
+scalacOptions ++= Seq("-encoding", "UTF-8")
+
---
+++ src/main/scala/CreateUsers.scala
@@ -0,0 +1,49 @@
+import com.amazonaws._
+import com.amazonaws.auth._
+import com.amazonaws.services.identitymanagement._
+import com.amazonaws.services.identitymanagement.model._
+import java.io.PrintWriter
+import scala.collection.JavaConversions._
+import scala.io.Source
+import scala.util.Random
+
+object CreateUsersMain extends App {
+  
+  val accessKey = "*****"
+  val secretKey = "*****"
+  val credentials = new BasicAWSCredentials(accessKey,secretKey)
+  val iam = new AmazonIdentityManagementClient(credentials)
+
+  def mkpwd() = {
+	val word = "ABCDEFGHJKELMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789"
+	val r = new Random()
+	r.shuffle(word.toList).take(8).mkString
+  }
+
+  var f = new PrintWriter("create-users-result.csv")
+
+  for (line <- Source.fromFile("create-users.csv").getLines) {
+	val lines = line.split("¥t|,")
+   	val groupName = lines(0)
+   	val userName = lines(1)
+	val password = lines(2) match {
+	  case "rand" => mkpwd()
+	  case _ => lines(2)
+	}
+	
+	println("try Create %s : %s : %s" format(groupName, userName, password))
+	
+	val user = iam.createUser(new CreateUserRequest().withUserName(userName))
+
+	println("    success : %s" format(user))
+
+	iam.addUserToGroup(new AddUserToGroupRequest(groupName, userName))	
+	iam.createLoginProfile(new CreateLoginProfileRequest(userName, password))
+
+	f.println("%s,%s,%s" format(groupName, userName, password))
+  }
+
+  f.flush
+  f.close
+
+}
